b0bced351856997cbfd12575eadd2f4638a71300
Rajesh Hegde <rhegde@datalicious.com>
2017-03-29 11:46:48 +0530

    columns update 	modified:   superset/migrations/versions/6f23fece6036_create_bigquery_tables.py

--- superset/migrations/versions/6f23fece6036_create_bigquery_tables.py
+++ superset/migrations/versions/6f23fece6036_create_bigquery_tables.py
@@ -17,7 +17,9 @@ import sqlalchemy as sa
 def upgrade():
     op.create_table('bigquery_table',
     sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('table_name', sa.String(length=255), nullable=False), #project_id:dataset_name.table_name
+    sa.Column('project_id', sa.String(length=255), nullable=False),
+    sa.Column('dataset_name', sa.String(length=255), nullable=False),
+    sa.Column('table_name', sa.String(length=255), nullable=False),
     sa.Column('is_featured', sa.Boolean(), nullable=True),
     sa.Column('filter_select_enabled', sa.Boolean(), nullable=True),
     sa.Column('description', sa.Text(), nullable=True),
@@ -26,17 +28,19 @@ def upgrade():
     sa.Column('cache_timeout', sa.Integer(), nullable=True),
     sa.Column('params', sa.String(length=1000), nullable=True),
     sa.Column('perm', sa.String(length=1000), nullable=True),
+    sa.Column('default_endpoint', sa.Text(), nullable=True),
+    sa.Column('metadata_last_refreshed', sa.DateTime(), nullable=True),
     sa.Column('created_on', sa.DateTime(), nullable=False),
     sa.Column('changed_on', sa.DateTime(), nullable=False),
     sa.Column('created_by_fk', sa.Integer(), sa.ForeignKey("ab_user.id"), nullable=True),
     sa.Column('changed_by_fk', sa.Integer(), sa.ForeignKey("ab_user.id"), nullable=True),
     sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('table_name')
+    sa.UniqueConstraint('project_id', 'dataset_name', 'table_name')
     )
 
     op.create_table('bigquery_column',
     sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('table_name', sa.String(length=255), sa.ForeignKey("bigquery_table.table_name"), nullable=False),
+    sa.Column('table_id', sa.Integer(), sa.ForeignKey("bigquery_table.id"), nullable=False),
     sa.Column('column_name', sa.String(length=255), nullable=True),
     sa.Column('verbose_name', sa.String(length=1024), nullable=True),
     sa.Column('is_active', sa.Boolean(), nullable=True),
@@ -62,7 +66,7 @@ def upgrade():
     sa.Column('metric_name', sa.String(length=512), nullable=True),
     sa.Column('verbose_name', sa.String(length=1024), nullable=True),
     sa.Column('metric_type', sa.String(length=32), nullable=True),
-    sa.Column('table_name', sa.String(length=255), sa.ForeignKey("bigquery_table.table_name"), nullable=False),
+    sa.Column('table_id', sa.Integer(), sa.ForeignKey("bigquery_table.id"), nullable=False),
     sa.Column('json', sa.Text(), nullable=True),
     sa.Column('description', sa.Text(), nullable=True),
     sa.Column('d3format', sa.String(length=128), nullable=True),
